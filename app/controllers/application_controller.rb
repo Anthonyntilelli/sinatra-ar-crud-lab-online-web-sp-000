3bff75c012274e2e31f9debe9dee7bbcfec4792e
Anthony Tilelli <anthony@tilelli.me>
2020-02-10 04:13:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/articles/:id/edit" do
-    @article = Article.find(params[:id])
+    id = params[:id]
+    @article = Article.find(id)
     erb :edit
   end
 
